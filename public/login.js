01913a95c3c0b83517331a342faf882ccd51dbf4
webdears <92512312+webdears@users.noreply.github.com>
2023-04-08 21:41:00 +0530

    Update login.js

--- public/login.js
+++ public/login.js
@@ -17,7 +17,7 @@ $("#signup").on("click",function(){
        
                 if(user!=''  && pass!=''){
                     $.ajax({
-                                url:"https://weather-1.vercel.app:1123/signup",:1123
+                                url:"https://weather-1.vercel.app:1123/signup",
                                 type:"post",
                                 dataType: "json",
                                 data: { User:user,Pass:pass
